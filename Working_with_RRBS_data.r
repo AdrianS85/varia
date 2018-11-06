2fc8d3f77a873bff7cb9011ea01aea9a5308eca8
AdrianS85 <adrianstankiewicz85@gmail.com>
2018-11-06 18:15:04 +0100

    Update Working_with_RRBS_data.r

--- Working_with_RRBS_data.r
+++ Working_with_RRBS_data.r
@@ -1,8 +1,62 @@
-install.packages("Matrix", "nlme", "mgcv", "xml2", "tidyverse")
+############ PREPARATION #####################################
+
 library(tidyverse)
 setwd("/media/adrians/USB DISK1/Projekty/FE - Federica/Sequencing/Liver_data")
 
+### Names of comparisons
 comparisons_ids <- c("b_vs_rest", "b_vs_c", "i_vs_rest", "i_vs_c", "e_vs_c")
+
+############ PREPARATION #####################################
+
+
+
+############ FUNCTIONS ####################################### 
+
+###### Function for getting lists of regions with p_val lower than 0.05 ######
+function_short_list <- function(tibble_list, region_type, comparison_number = 5, comparison_names= comparisons_ids){
+### tibble_list = list of tibbles from RnBeads differential module results
+### region_type = name of region, string
+### comparison_names = string vector of the same number of elements as comparison_number
+    
+  ### Initialize list, so I can work on it
+  annotated_tibble_list <- list()
+  ### Establish appropriate amount of for loops
+  loop_number <- seq(from = 1, to = comparison_number)
+  
+  ### Add comparison names to tables
+  for(n in loop_number){
+    annotated_tibble_list[[n]] <- tibble_list[[n]] %>%  ## As tibble
+      mutate(
+        Comparison = str_replace(string = Chromosome, pattern = ".*", replacement = comparison_names[n]) ### Add column of appropriate size and change all its values into name of the comparison
+      ) %>%
+      select(Comparison, everything()) ### Select all the columns, but put comparison id column at the begininng
+  }
+  
+  ### Select only p.val lower than 0.05.
+  pval_list <- annotated_tibble_list  %>%  
+    map(filter, comb.p.val <= 0.05) 
+  
+  return(pval_list)  
+}
+
+
+
+###### Function for writing tables from lists of regions ######
+### tibble_list = list of tibbles from RnBeads differential module results, analyzed as neccecary
+### region_type = name of region, string
+function_write_list <- function(annotated_tibble_list, region_type, comparison_number = 5){
+  
+  ### Establish appropriate amount of for loops
+  loop_number <- seq(from = 1, to = comparison_number)
+  
+  for(n in loop_number){
+    write.table(annotated_tibble_list[n], paste(annotated_tibble_list[[n]]$Comparison[1], "_", region_type, ".txt", sep = ""), sep="\t")
+  } 
+  
+  return()  
+}
+
+############ FUNCTIONS ####################################### 
 
 
 
@@ -10,31 +64,14 @@ comparisons_ids <- c("b_vs_rest", "b_vs_c", "i_vs_rest", "i_vs_c", "e_vs_c")
 
 ### Here I upload comparisons
 gene_tibble_list <- lapply(list.files(pattern = "*genes.csv"), FUN = read_csv, col_names = T, col_types = "ccnnccnnnnnnnnnnnnnn") 
+
 ### Here I checked is the comparisons are uploaded in order, and it seems that they indeed are
 names <- list.files(pattern = "*genes.csv") 
 
-
-
-### Initialize list, so I can work on it
-annotated_tibble_list <- list() 
-
-### Add comparison names to tables
-for(n in 1:5){
-  annotated_tibble_list[[n]] <- gene_tibble_list[[n]] %>%  ## As tibble
-  mutate(
-    Comparison = str_replace(string = Chromosome, pattern = ".*", replacement = comparisons_ids[n]) ### Add column of appropriate size and change all its values into name of the comparison
-  ) %>%
-  select(Comparison, everything()) ### Select all the columns, but put comparison id column at the begininng
-}
-
-### Select only p.val lower than 0.05.
-gene_pval_list <- annotated_tibble_list  %>%  
-  map(filter, comb.p.val <= 0.05) 
-
-
+### Get p.val loerw than 5 tables
+gene_pval_list <- function_short_list(tibble_list = gene_tibble_list, region_type = "genes")
 
 ### Write the tables
-for(n in 1:5){
-  write.table(gene_pval_list[n], paste(gene_pval_list[[n]]$Comparison[1], "gene.txt", sep = "_"), sep="\t")
-}
+function_write_list(gene_pval_list, "genes")
+
 ############ GENE REGIONS #################################### 
