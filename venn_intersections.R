a353a7fb6f8dbf12d5929ca77433261d8f602dad
AdrianS85 <adrianstankiewicz85@gmail.com>
2019-01-22 20:27:21 +0100

    Update venn_intersections.R

--- venn_intersections.R
+++ venn_intersections.R
@@ -5,7 +5,7 @@ install.packages("VennDiagram")
 
 
 # Here we upload files with gene names (column 2) and comparison name (column 1). One file for each comparison
-genes_list <- lapply(list.files(pattern = "*brain_.txt"), FUN = read.table, header = T) 
+genes_list <- lapply(list.files(pattern = "*placenta_.txt"), FUN = read.table, header = T) # liver placenta brain
 
 
 # Here we produce list of character vectors to venn (from column 2 of each file) and names (from column 1 of each file). We need named list for intersection comparison
@@ -19,5 +19,6 @@ names(vector_list) <- name_vector
 
 # Here is the crucial part: We produce list of genes belonging to each intersection of comparisons!
 inters <- attr(gplots::venn(vector_list, show.plot=FALSE),"intersections")
-rlist::list.save(x = inters, file = "intersections_names_list.json")
-# convert to excel here: https://json-csv.com/
+rlist::list.save(x = inters, file = "placenta_intersections_names_list.json")
+# liver_ placenta_ brain_
+# convert json to excel here https://json-csv.com/
