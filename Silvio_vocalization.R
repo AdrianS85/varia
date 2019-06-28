69eeae2f5fe72e6cab91af866d29582e3a7f3c23
AdrianS85 <adrianstankiewicz85@gmail.com>
2019-06-28 19:59:45 +0200

    Create Silvio_vocalization.R

---
+++ Silvio_vocalization.R
@@ -0,0 +1,20 @@
+setwd("E:/Projekty/SV - Silvio Vocalization")
+
+library(readxl)
+library(tidyverse)
+library(rlist)
+
+path = "USVs BTBR-C57 - SONATA.xlsx"
+
+xl_list <- lapply(excel_sheets(path), read_excel, path = path)
+
+named_xl_list <- map2(.x = xl_list, .y = excel_sheets(path), .f = ~ mutate(.x, mouse = .y ) )
+
+named_xl_df <- rlist::list.rbind(.data = named_xl_list)
+
+grouped_named_xl_df <- named_xl_df %>%
+  mutate( group = str_replace(mouse, "[0-9]+", "") )
+
+grouped_named_xl_df %>%
+  ggplot(aes(x = `Call #`, y = mouse, color = group))+
+  geom_point()
