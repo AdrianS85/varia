9f37f76a675aa7ad68ad610fa12778d30a89bcf6
AdrianS85 <adrianstankiewicz85@gmail.com>
2019-06-28 20:02:07 +0200

    Update Silvio_vocalization.R

--- Silvio_vocalization.R
+++ Silvio_vocalization.R
@@ -15,6 +15,9 @@ named_xl_df <- rlist::list.rbind(.data = named_xl_list)
 grouped_named_xl_df <- named_xl_df %>%
   mutate( group = str_replace(mouse, "[0-9]+", "") )
 
+readr::write_tsv(grouped_named_xl_df, path = "grouped_named_xl_df_USVs_BTB_C57_SONATA.tsv")
+
 grouped_named_xl_df %>%
   ggplot(aes(x = `Call #`, y = mouse, color = group))+
   geom_point()
+
