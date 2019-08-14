06c4ff1411188e8fc5db95a7eb96b4656d680679
AdrianS85 <adrianstankiewicz85@gmail.com>
2019-08-14 20:15:39 +0200

    Update Accessing_google_drive.py

--- Accessing_google_drive.py
+++ Accessing_google_drive.py
@@ -7,8 +7,8 @@ from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from oauth2client import file, client, tools
 
 scopes = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-client_id = '547846840425-0f63erbq9s1vtr9htm6j5l8d4ujr8gck.apps.googleusercontent.com'
-client_secret = 'tMCbjTRP5FStZCOEfS2EMKj1'
+client_id = '***.googleusercontent.com'
+client_secret = '***j1'
 # store = file.Storage('storage.json')
 
 # flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file = 'client_secret.json',  scopes = SCOPES)
